ee797e79c8aa810855856a6891afb2459ff53c60
yunik1004 <inkyuhak@gmail.com>
2022-11-12 23:01:30 +0900

    Feat: Add contents of publication

--- src/routes/about/item_publication.svelte
+++ src/routes/about/item_publication.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	export let title: string;
+	export let journal: string;
 </script>
 
-<div>
+<div class="pb-5">
 	<h2 class="text-2xl font-bold">{title}</h2>
+	<p class="font-serif italic opacity-50">{journal}</p>
+	<slot />
 </div>
--- src/routes/about/publication.svelte
+++ src/routes/about/publication.svelte
@@ -18,9 +18,64 @@
 		/>
 	</svg>
 
-	<h2 class="text-3xl font-bold pl-3">Publication</h2>
+	<h2 class="text-3xl font-bold pl-3 pb-10">
+		Publication <small class="text-base">(*=Equal Contribution)</small>
+	</h2>
 </div>
 
 <div>
-	<ItemPublication title="Test" />
+	<ItemPublication
+		title="Efficient Neural Network Approximation of Robust PCA for Automated Analysis of Calcium Imaging Data"
+		journal="24th International Conference on Medical Image Computing & Computer Assisted Intervention (MICCAI ’21)"
+	>
+		<p class="opacity-75">
+			Seungjae Han, Eun-seo Cho, <u>Inkyu Park</u>, Kijung Shin, and Young-gyu Yoon
+		</p>
+		<p class="opacity-50">
+			[
+			<a class="link link-hover" href="https://arxiv.org/abs/2108.01665">paper</a>
+			|
+			<a class="link link-hover" href="https://github.com/NICALab/BEAR">code and datasets</a>
+			|
+			<a
+				class="link link-hover"
+				href="https://dblp.uni-trier.de/rec/conf/miccai/HanCPSY21.html?view=bibtex"
+				>bib
+			</a>
+			]
+		</p>
+	</ItemPublication>
+	<ItemPublication
+		title="SliceNStitch: Continuous CP Decomposition of Sparse Tensor Streams"
+		journal="37th IEEE International Conference on Data Engineering (ICDE ’21)"
+	>
+		<p class="opacity-75">Taehyung Kwon*, <u>Inkyu Park</u>*, Dongjin Lee, and Kijung Shin</p>
+		<p class="opacity-50">
+			[
+			<a class="link link-hover" href="https://arxiv.org/abs/2102.11517">paper</a>
+			|
+			<a
+				class="link link-hover"
+				href="https://github.com/DMLab-Tensor/SliceNStitch/blob/master/doc/supplementary.pdf"
+				>appendix
+			</a>
+			|
+			<a class="link link-hover" href="https://youtu.be/x5yiWLvPCFo">video</a>
+			|
+			<a class="link link-hover" href="http://dmlab.kaist.ac.kr/~kijungs/snsICDE2021.pdf"
+				>slides
+			</a>
+			|
+			<a class="link link-hover" href="https://github.com/DMLab-Tensor/SliceNStitch"
+				>code and datasets
+			</a>
+			|
+			<a
+				class="link link-hover"
+				href="https://dblp.uni-trier.de/rec/conf/icde/Kwon0LS21.html?view=bibtex"
+				>bib
+			</a>
+			]
+		</p>
+	</ItemPublication>
 </div>
