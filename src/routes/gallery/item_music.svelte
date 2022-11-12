f56ab1ff1af0070758f016f089677c7adc9fd449
yunik1004 <inkyuhak@gmail.com>
2022-11-13 01:07:49 +0900

    Style: Change the location of the music player

--- src/routes/gallery/item_music.svelte
+++ src/routes/gallery/item_music.svelte
@@ -5,8 +5,12 @@
 	export let src: string;
 </script>
 
-<div class="pb-5">
-	<h2 class="text-2xl font-bold">{title}</h2>
-	<p class="font-serif italic opacity-50">{credit}, {date}</p>
-	<audio {src} controls class="bg-base-content" />
+<div class="md:flex pb-5">
+	<div class="flex-none">
+		<h2 class="text-2xl font-bold">{title}</h2>
+		<p class="font-serif italic opacity-50">{credit}, {date}</p>
+	</div>
+	<div class="flex-1 md:pl-5">
+		<audio {src} controls class="bg-base-content" />
+	</div>
 </div>
