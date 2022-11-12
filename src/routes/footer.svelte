44f05c09ea3c775860230b2dd77ccd5e0edea31b
yunik1004 <inkyuhak@gmail.com>
2022-11-12 15:18:31 +0900

    Fix: Svelte's script lang="ts"

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	let current_year = new Date().getUTCFullYear();
 </script>
 
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,1 +1,8 @@
+<script lang="ts">
+	import ItemProject from './item_project.svelte';
+</script>
+
 <h1 class="text-3xl font-bold">Project</h1>
+
+<ItemProject title="hello" />
+<ItemProject title="how are you" />
