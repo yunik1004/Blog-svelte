094a88ebc8edc7072d4698494951205b57adc416
yunik1004 <inkyuhak@gmail.com>
2024-02-13 12:37:52 +0900

    Feat: Add game section
    - Add suika game

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import Art from './Art.svelte';
+	import Game from './Game.svelte';
 	import Music from './Music.svelte';
 </script>
 
 <div>
+	<Game />
+	<div class="divider" />
 	<Music />
 	<div class="divider" />
 	<Art />
---
+++ src/routes/gallery/Game.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Card from '$lib/Card.svelte';
+	import ItemGame from './ItemGame.svelte';
+</script>
+
+<Card title="Game" symbol="game">
+	<ItemGame
+		title="SuikaGamePlus"
+		detail="Replicated version of Suika game using Godot engine 4"
+		image="images/game/suika_game_plus.svg"
+		download="https://yunik1004.itch.io/suikagameplus"
+		code="https://github.com/yunik1004/SuikaGamePlus"
+	/>
+</Card>
