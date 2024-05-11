5db2a0c5a1f7ba33e336c9e0c9483dd14b467600
yunik1004 <inkyuhak@gmail.com>
2024-05-11 19:43:27 +0900

    Feat: Change locations of publication, preprint to project section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import Education from "./Education.svelte";
-  import Preprint from "./Preprint.svelte";
-  import Publication from "./Publication.svelte";
   import Teach from "./Teach.svelte";
   import Work from "./Work.svelte";
 </script>
@@ -12,8 +10,4 @@
   <Work />
   <div class="divider" />
   <Teach />
-  <div class="divider" />
-  <Preprint />
-  <div class="divider" />
-  <Publication />
 </div>
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+  import Preprint from "./Preprint.svelte";
   import Project from "./Project.svelte";
+  import Publication from "./Publication.svelte";
 </script>
 
-<Project></Project>
+<div>
+  <Publication />
+  <div class="divider" />
+  <Preprint />
+  <div class="divider" />
+  <Project />
+</div>
