48b3030994d13443293bfae03f3b2c22a7681c03
inkyupark <inkyupark@krafton.com>
2025-06-09 13:49:36 +0900

    Feat: Add preprint

--- src/routes/project/Preprint.svelte
+++ src/routes/project/Preprint.svelte
@@ -5,6 +5,19 @@
 
 <Card title="Preprint" symbol="paper">
   <ItemPublication
+    title="Orak: A Foundational Benchmark for Training and Evaluating LLM Agents on Diverse Video Games"
+    journal="arXiv preprint (2025)"
+    author="Dongmin Park, Minkyu Kim, Beongjun Choi, Junhyuck Kim, Keon Lee, Jonghyun Lee, Inkyu Park, Byeong-Uk Lee, Jaeyoung Hwang, Jaewoo Ahn, Ameya S. Mahabaleshwarkar, Bilal Kartal, Pritam Biswas, Yoshi Suhara, Kangwook Lee, Jaewoong Cho"
+  >
+    [
+    <a class="link link-hover" href="https://arxiv.org/abs/2506.03610">paper</a>
+    |
+    <!--<a class="link link-hover" href="https://yunik1004.github.io/SAiD">project page</a>
+    |-->
+    <a class="link link-hover" href="https://github.com/krafton-ai/Orak">code and datasets</a>
+    ]
+  </ItemPublication>
+  <ItemPublication
     title="SAiD: Speech-driven Blendshape Facial Animation with Diffusion"
     journal="arXiv preprint (2023)"
     author="Inkyu Park, Jaewoong Cho"
